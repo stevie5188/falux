0fe237e63aaee2679355a5c63cbe604fd0f2763d
stevie5188 <32532207+stevie5188@users.noreply.github.com>
2022-05-23 05:39:44 +0000

    Update script.js
    removed console logs

--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@
 function imageSize(windowHeight) {
     if (windowHeight >= 1080) {
         document.querySelector('body').style.backgroundImage = 'url("../media/FaluxWEB4k.jpg")';
-        console.log('4k');
     }   else {
         document.querySelector('body').style.backgroundImage = 'url("../media/FaluxWEB1080.jpg")';
-        console.log('1080');
     }
 }
 document.addEventListener('DOMContentLoaded', () => {
